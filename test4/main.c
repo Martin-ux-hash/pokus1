3194d9c37513ff974ff62a8efcebbf6964f1f040
Kulik Martin <kulik.martin@icloud.com>
2016-11-25 21:00:19 +0100

    Initial Commit

---
+++ test4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test4
+//
+//  Created by Martin Kulik on 25.11.16.
+//  Copyright © 2016 ReddishBlue. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
