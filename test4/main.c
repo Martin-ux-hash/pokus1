182d5882c14bb1dc662d20389df127cc1e1f9f2f
Martin <martin>
2025-10-15 20:19:22 +0200

    Init commit

--- test4/main.c
+++ test4/main.c
@@ -2,14 +2,19 @@
 //  main.c
 //  test4
 //
-//  Created by Martin Kulik on 25.11.16.
-//  Copyright © 2016 ReddishBlue. All rights reserved.
-//
 
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void) {
+    
+    int i;
+    
+    for (i=1; i<=10; i++) {
+        printf("%0*d\n",6,i * i);
+        printf("%05o\n", i * i);
+    }
+    
+    printf("Slepice dělá %*.*s.\n", 30, 4, "kokokodák");
     return 0;
 }
